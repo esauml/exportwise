e30b5b982fd1a4513c75cddc57deb6c8e543eb34
Angel <c3po.angel@gmail.com>
2021-03-25 17:51:19 +0100

    modifiy home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,14 +16,34 @@ class HomeController extends AbstractController
     {
         $em = $this->getDoctrine()->getManager();
         $data = $em->getRepository(Product::class)->findAll();
-        $product = sizeof($data) - 1;
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'slider' => [
-                'price' => $data[$product]->getPrice(),
-                'name' => $data[$product]->getName(),
-                'description' => $data[$product]->getDescription(),
-            ],
-        ]);
+        $query = $em->createQuery(
+            'SELECT DISTINCT e.country
+            FROM App\Entity\Enterprise e'
+        );
+        if(sizeof($data)>0){
+            $product = sizeof($data) - 1;
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController',
+                'slider' => [
+                    'price' => $data[$product]->getPrice(),
+                    'name' => $data[$product]->getName(),
+                    'description' => $data[$product]->getDescription(),
+                ],
+                'countries' => $query->getResult()
+            ]);
+        }
+        else{
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController',
+                'slider' => [
+                    'price' => '',
+                    'name' =>'',
+                    'description' => '',
+                ],
+                'countries' => $query->getResult()
+            ]);
+        }
+       
+        
     }
 }
